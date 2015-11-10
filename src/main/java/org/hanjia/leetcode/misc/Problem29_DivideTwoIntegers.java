67beb8e0f694e9772c67f94ccd92fe2205a981e6
Han Jia <hajia@ebay.com>
2015-11-10 09:55:04 -0800

    Nov 9th

--- src/main/java/org/hanjia/leetcode/misc/Problem29_DivideTwoIntegers.java
+++ src/main/java/org/hanjia/leetcode/misc/Problem29_DivideTwoIntegers.java
@@ -1,28 +1,47 @@
 package org.hanjia.leetcode.misc;
 
+/**
+ * 
+ * Divide two integers without using multiplication, division and mod operator.
+ * 
+ * If it is overflow, return MAX_INT.
+ * 
+ * number = a0 * 2^0 + a1 * (2^1) + a2 * (2^2) + ... + an * (2^n)
+ * 
+ * @author hanjia
+ *
+ */
 public class Problem29_DivideTwoIntegers {
-    public int divide(int dividend, int divisor) {
+    public static int divide(int dividend, int divisor) {
         if(divisor == 0 || (divisor == -1 && dividend == Integer.MIN_VALUE))
             return Integer.MAX_VALUE;
     
-        long remain = Math.abs((long)dividend);
-        long div = Math.abs((long)divisor);
-        int count = 0;
+        long remain = Math.abs((long) dividend);
+        long div = Math.abs((long) divisor);
+        int result = 0;
         
         while(remain >= div){
         	 int numShift = 0;    
-             while(remain>=(div<<numShift)){
+             while(remain >= (div << numShift)){
                  numShift++;
-             }
-      
+             }     
+             result += (1 << (numShift-1));
              //dividend minus the largest shifted divisor
-             count += 1<< (numShift-1);
              remain -= (div << (numShift-1));
         }
         if((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)){
-            return -count;
+            return -result;
         }else{
-            return count;
+            return result;
         }
+    }
+    
+    public static void main(String[] args){
+    	System.out.println(divide(2147483647, 1));
+    	System.out.println(divide(2147483647, -1));
+    	System.out.println(divide(1, -1));
+    	System.out.println(divide(1, 0));
+    	System.out.println(divide(0, 1));
+    	System.out.println(1 << 3);
     }
 }
---
+++ src/main/java/org/hanjia/leetcode/string/Problem30_SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,86 @@
+package org.hanjia.leetcode.string;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * You are given a string, s, and a list of words, words, that are all of the same length. 
+ * Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+ * 
+ * For example,
+ * s: "barfoothefoobarman"
+ * words: ["foo", "bar"]
+ * 
+ * You should return the indices: [0,9]. (order does not matter).
+ * 
+ * @author hanjia
+ *
+ */
+public class Problem30_SubstringWithConcatenationOfAllWords {
+	
+	public static List<Integer> findSubstring(String s, String[] words) {
+		List<Integer> result = new ArrayList<Integer>();
+		if(s == null || s.length()==0 || words == null || words.length == 0)  
+	        return result;
+		
+        int wordLength = words[0].toCharArray().length;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for(String word: words){
+        	if(map.containsKey(word))
+        		map.put(word, map.get(word) + 1);
+        	else
+        		map.put(word, 1);
+        }
+        
+        for(int i = 0; i < wordLength; i++){
+            HashMap<String, Integer> currentMap = new HashMap<String, Integer>();
+            int start = i; // starting index
+            int count = 0; // number of qualified words
+            
+            for(int j = i; j <= s.length() - wordLength; j += wordLength){
+            	String subString = s.substring(j, j + wordLength);
+            	if(map.containsKey(subString)){
+            		if(currentMap.containsKey(subString)){
+                        currentMap.put(subString, currentMap.get(subString) + 1);
+                    }else{
+                        currentMap.put(subString, 1);
+                    }
+            		count++;
+            		while(currentMap.get(subString) > map.get(subString)){
+                        String temp = s.substring(start, start + wordLength);
+                        if(currentMap.containsKey(temp)){
+                        	currentMap.put(temp, currentMap.get(temp) - 1);
+                        }
+                        count--;
+                        start += wordLength;
+                    }
+            		
+            		if(count==words.length){
+                        result.add(start); // add to result    
+                        String temp = s.substring(start, start + wordLength); // shift right and reset currentMap, count & start point         
+                        if(currentMap.containsKey(temp)){
+                        	currentMap.put(temp, currentMap.get(temp) - 1);
+                        }
+                        count--;
+                        start += wordLength;
+                    }           		
+            	}else{
+                    currentMap.clear();
+                    start = j + wordLength;
+                    count = 0;           		
+            	}
+            }
+        }
+               
+        return result;
+    }
+	
+	public static void main(String[] args){
+		String s = "barfoothefoobarman";
+		String[] words = {"foo","bar"};
+		System.out.println(findSubstring(s, words));
+	}
+}
