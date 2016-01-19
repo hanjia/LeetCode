e6f769c2528212e6787feabb021f26431068fd9e
Han Jia <hajia@ebay.com>
2016-01-18 21:56:40 -0800

    Jan 18th

--- src/main/java/org/hanjia/leetcode/dynamicprogramming/Problem5_LongestPalindromicString.java
+++ src/main/java/org/hanjia/leetcode/dynamicprogramming/Problem5_LongestPalindromicString.java
@@ -11,13 +11,18 @@ package org.hanjia.leetcode.dynamicprogramming;
  *
  */
 public class Problem5_LongestPalindromicString {
-	
-	public String longestPalindromeThroughDP (String str) {
+	public String longestPalindromeDPSolution (String str) {
 	    boolean[][] table = new boolean[str.length()][str.length()];
 	    int max = 0;
 	    String longest = null;
 	    for (int i = str.length() - 1; i >= 0; i--) {
 	        for (int j = i; j < str.length(); j++) {
+	        	/**
+	        	 * For j >= i
+	        	 * LPS[i, j] = true if LPS[i+1][j-1] is true and character i and j are the same
+	        	 * Special case is the distance of i and j is smaller than 2
+	        	 * 
+	        	 */
 	            if (str.charAt(i) == str.charAt(j) && ((j - i <= 2) || table[i + 1][j - 1])) {
 	                table[i][j] = true;
 	                if (max < j - i + 1) {
@@ -59,6 +64,6 @@ public class Problem5_LongestPalindromicString {
 	public static void main(String[] args){
 		String str = "dawabcbawefrwp";
 		Problem5_LongestPalindromicString longestPalindromicString = new Problem5_LongestPalindromicString();
-		System.out.println(longestPalindromicString.longestPalindromeThroughDP(str));		
+		System.out.println(longestPalindromicString.longestPalindromeDPSolution(str));
 	}
 }
