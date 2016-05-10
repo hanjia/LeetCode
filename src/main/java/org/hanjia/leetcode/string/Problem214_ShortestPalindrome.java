a86878dc89b90de3cf4e55ae8aafcd9ccebe8136
Han Jia <hajia@ebay.com>
2016-05-09 23:35:40 -0700

    additional problems

---
+++ src/main/java/org/hanjia/leetcode/string/Problem214_ShortestPalindrome.java
@@ -0,0 +1,19 @@
+package org.hanjia.leetcode.string;
+
+/**
+ * 
+ * Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it. Find and return the shortest palindrome you can find by performing this transformation.
+ * For example:
+ * 
+ * Given "aacecaaa", return "aaacecaaa".
+ * Given "abcd", return "dcbabcd".
+ * 
+ * 
+ * @author hajia
+ *
+ */
+public class Problem214_ShortestPalindrome {
+
+	// TODO
+	
+}
