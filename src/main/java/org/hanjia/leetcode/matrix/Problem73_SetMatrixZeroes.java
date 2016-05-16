faa1c2f074d0f462e866bc049c4c94e347b7bdb4
Han Jia <hajia@ebay.com>
2016-05-15 23:32:02 -0700

    matrix problems

--- src/main/java/org/hanjia/leetcode/matrix/Problem73_SetMatrixZeroes.java
+++ src/main/java/org/hanjia/leetcode/matrix/Problem73_SetMatrixZeroes.java
@@ -14,53 +14,52 @@ package org.hanjia.leetcode.matrix;
  */
 public class Problem73_SetMatrixZeroes {
 	 public void setZeroes(int[][] matrix) {
-	        boolean firstRowZero = false;
-	        boolean firstColumnZero = false;
+	    boolean zeroInFirstRow = false;
+	    boolean zeroInFirstColumn = false;
 	 
-	        //set first row and column zero or not
-	        for(int i=0; i<matrix.length; i++){
-	            if(matrix[i][0] == 0){
-	                firstColumnZero = true;
-	                break;
-	            }
-	        }
+	    // set first row and column zero or not
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[i][0] == 0) {
+				zeroInFirstColumn = true;
+				break;
+			}
+		}
 	 
-	        for(int i=0; i<matrix[0].length; i++){
-	            if(matrix[0][i] == 0){
-	                firstRowZero = true;
-	                break;
-	            }
-	        }
+		for (int i = 0; i < matrix[0].length; i++) {
+			if (matrix[0][i] == 0) {
+				zeroInFirstRow = true;
+				break;
+			}
+		}
 	 
-	        //mark zeros on first row and column
-	        for(int i=1; i<matrix.length; i++){
-	            for(int j=1; j<matrix[0].length; j++){
-	                if(matrix[i][j] == 0){
-	                   matrix[i][0] = 0;
-	                   matrix[0][j] = 0;
-	                }
-	            }
-	        }
+		// important: mark zeroes on first row and column
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 1; j < matrix[0].length; j++) {
+				if (matrix[i][j] == 0) {
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+				}
+			}
+		}
 	 
-	        //use mark to set elements
-	        for(int i=1; i<matrix.length; i++){
-	            for(int j=1; j<matrix[0].length; j++){
-	                if(matrix[i][0] == 0 || matrix[0][j] == 0){
-	                   matrix[i][j] = 0;
-	                }
-	            }
-	        }
+		// use mark to set elements
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 1; j < matrix[0].length; j++) {
+				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
 	 
-	        //set first column and row
-	        if(firstColumnZero){
-	            for(int i=0; i<matrix.length; i++)
-	                matrix[i][0] = 0;
-	        }
-	 
-	        if(firstRowZero){
-	            for(int i=0; i<matrix[0].length; i++)
-	                matrix[0][i] = 0;
-	        }
-	 
-	    }
+		// set first column and row
+		if (zeroInFirstColumn) {
+			for (int i = 0; i < matrix.length; i++)
+				matrix[i][0] = 0;
+		}
+
+		if (zeroInFirstRow) {
+			for (int i = 0; i < matrix[0].length; i++)
+				matrix[0][i] = 0;
+		}
+	 }
 }
