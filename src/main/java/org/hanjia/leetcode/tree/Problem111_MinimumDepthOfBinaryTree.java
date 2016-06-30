fc0434caa75a6ab6090498ddc329875df5ef2092
Han Jia <hajia@ebay.com>
2016-06-30 10:45:20 -0700

    add comments

--- src/main/java/org/hanjia/leetcode/tree/Problem111_MinimumDepthOfBinaryTree.java
+++ src/main/java/org/hanjia/leetcode/tree/Problem111_MinimumDepthOfBinaryTree.java
@@ -7,7 +7,7 @@ public class Problem111_MinimumDepthOfBinaryTree {
         }
         int left = minDepth(root.left);
         int right = minDepth(root.right);
-        if (left == 0 || right == 0) {
+        if (left == 0 || right == 0) { // If one subtree is null, we cannot return 0 but the depth of the other subtree
             return left > right ? left + 1: right + 1;
         }
         return 1 + Math.min(left, right);
